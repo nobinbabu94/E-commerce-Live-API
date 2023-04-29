55b1231361550bae5296f42e9d2b59ec70eb7430
Nobin Babu <nobins73@gmail.com>
2023-04-30 02:59:49 +0530

    body,right clck evnt lstner added(cant right clck)

--- src/components/About.js
+++ src/components/About.js
@@ -22,16 +22,16 @@ class About extends React.Component {
     render() {
         console.log('Parent inside render')
         return (
-            <>
+            <div className='p-5 flex-col items-center justify-center'>
                 <h1>{ }</h1>
                 <h1 class="text-3xl font-bold underline">
                     Hello world!
                 </h1>
-                <button onClick={() => this.setState({ count: 1 })}>Add</button>
+                <button className='bg-red-600 rounded w-24 h-8' onClick={() => this.setState({ count: 1 })}>Add</button>
                 <h1>{this.state.count}</h1>
                 <Profile xyz={'hello'} />
 
-            </>
+            </div>
         )
     }
 }
